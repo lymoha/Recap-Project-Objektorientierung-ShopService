baf98ade343401c20def189faca65c81e0ac0717
Mohammed Ly <lybhoye@yahoo.com>
2024-06-13 15:57:59 +0200

    Test no more in use

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -5,32 +5,32 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopServiceTest {
-
-    @Test
-    void addOrderTest() {
-        //GIVEN
-        ShopService shopService = new ShopService();
-        List<String> productsIds = List.of("1");
-
-        //WHEN
-        Order actual = shopService.addOrder(productsIds);
-
-        //THEN
-        Order expected = new Order("-1", List.of(new Product("1", "Apfel")));
-        assertEquals(expected.products(), actual.products());
-        assertNotNull(expected.id());
-    }
-
-    @Test
-    void addOrderTest_whenInvalidProductId_expectNull() {
-        //GIVEN
-        ShopService shopService = new ShopService();
-        List<String> productsIds = List.of("1", "2");
-
-        //WHEN
-        Order actual = shopService.addOrder(productsIds);
-
-        //THEN
-        assertNull(actual);
-    }
+//
+//    @Test
+//    void addOrderTest() {
+//        //GIVEN
+//        ShopService shopService = new ShopService();
+//        List<String> productsIds = List.of("1");
+//
+//        //WHEN
+//        Order actual = shopService.addOrder(productsIds);
+//
+//        //THEN
+//        Order expected = new Order("-1", List.of(new Product("1", "Apfel")));
+//        assertEquals(expected.products(), actual.products());
+//        assertNotNull(expected.id());
+//    }
+//
+//    @Test
+//    void addOrderTest_whenInvalidProductId_expectNull() {
+//        //GIVEN
+//        ShopService shopService = new ShopService();
+//        List<String> productsIds = List.of("1", "2");
+//
+//        //WHEN
+//        Order actual = shopService.addOrder(productsIds);
+//
+//        //THEN
+//        assertNull(actual);
+//    }
 }
